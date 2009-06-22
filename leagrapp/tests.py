2cc52460e46c197efc73fd8e4f3323717ec164c6
Stuart A. Doherty <stu.doherty@gmail.com>
2009-06-22 09:50:12 -0400

    Some changes to tests, but checking in prior to integrating GAE 1.2.3, and AEP 1.0.2.1

--- leagrapp/tests.py
+++ leagrapp/tests.py
@@ -9,13 +9,36 @@ def createLeague(name='testleague'):
     league.put()
     return league
 
-def createDivision(name, league):
+def createDivision(name, league, num_teams, num_games, num_finished_games):
     """ Creates a test division within the given league. \
     Returns the pk for the created division."""
     division = Division(title=name, league=league)
     division.put()
     return division
+
+def createSchedule(team_set, start_date, num_games):
+    """Creates a season of games based on the input parameters"""
+    pass
+
+def createWholeLeague(name, num_divisions, num_teams, num_games, num_finished_games):    
+    """ Method creates and stores all the objects relevant to a particular 
+    league setup
+    Inputs:
+    num_divisions - Number of distinct divisions to create
+    num_teams - Number of teams in each division
+    num_games - Number of games played by each team in the season
+    num_finished_games - Number of games that have already completed
+    """
+    league = createLeague(name)
+    divisions = []
     
+    data_set = {}
+    for d in range(num_divisions):
+        division_name = "Div%d_%s" % (d, league.name)
+        div = createDivision(division_name, league)
+        divisions.append(div)
+
+
 ## League Specific Tests
 #class TeamTestCase(unittest.TestCase):django.test.TestCase
 class LeagueTestCase(django.test.TestCase):
@@ -67,3 +90,21 @@ class DivisionTestCase(django.test.TestCase):
         d = Division.all().fetch(1)[0]
         self.assertEquals(len(d.division_team_set), len(self.team_list))
 
+class GameEditTestCase(django.test.TestCase):
+    def setUp(self):
+        self.team_list = ['Giraffe', 'Dogs']
+        self.league_name = "testleague"
+        self.league = createLeague(name=self.league_name)
+        self.division_name = "premier"
+        self.division = createDivision(self.division_name, self.league)
+
+        # Create teams in database
+        self.teams = []
+        for t in self.team_list:
+            #print "Adding team: %s" % t
+            new_team = Team(name=t, division=self.division)
+            new_team.put()
+            self.teams.append(new_team)
+
+
+        
